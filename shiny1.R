dc1e381bf9b60d2bd47acdb70452b786449325d6
LorenaLuraghi <loreluraghi@hotmail.com>
2018-05-31 20:58:00 -0300

    con ejemplo de tabla

--- shiny1.R
+++ shiny1.R
@@ -1,7 +1,11 @@
 library(shiny)
 library(tidyverse)
 library(lubridate)
-library(interval)
+
+temperaturas <- read.csv("temperaturas.csv",sep="\t")
+temperaturas <- mutate(temperaturas, fecha=paste(dia,mes,anio, sep="-"))
+temperaturas <- mutate(temperaturas,fecha=dmy(temperaturas$fecha))
+
 
 ui <- fluidPage(
   titlePanel("Temperaturas del Uru"),
@@ -14,10 +18,17 @@ ui <- fluidPage(
               label = "Año",
               min =2004,
               max = 2015,
-              value =c( 2002,2004)
+              value =c( 2002:2004)
   ),
+  sliderInput(inputId ="m",
+              label = "superior",
+              min = -10,
+              max=30,
+              value=12,
+              step= 0.2 ),
   plotOutput("serie")
-)
+  
+  )
 
 
 server <- function(input,output){
@@ -26,15 +37,35 @@ server <- function(input,output){
     temperaturas <- mutate(temperaturas, fecha=paste(dia,mes,anio, sep="-"))
     temperaturas <- mutate(temperaturas,fecha=dmy(temperaturas$fecha))
     temperaturas %>% filter(nroEstacion==input$est) %>% filter(between(anio,min(input$year),max(input$year))) %>%
-      ggplot(aes(x=fecha,y=tmin,colour=as.factor(anio))) + geom_point()
-    
-    
-    
+      ggplot(aes(x=fecha,y=tmin,colour=as.factor(anio))) + geom_point()+geom_hline(yintercept = input$m, colour="red",size=2)+
+      ggtitle(paste("Temperaturas de la estación N°",input$est))
   })
 }
 
 
+
 shinyApp(ui, server)
 
+
+
+#ejemplo tabla 
+
+library(shiny)
+library(tidyverse)
+temperaturas <- read.csv("temperaturas.csv",sep="\t")
+temperaturas <- mutate(temperaturas, fecha=paste(dia,mes,anio, sep="-"))
+temperaturas <- mutate(temperaturas,fecha=dmy(temperaturas$fecha))
+
+ui <- fluidPage(
+  fluidRow(column(4,dataTableOutput('dto')))
+)
+
+server <- function(input,output){
+  
+  output$dto <- renderDataTable({filter(temperaturas,nroEstacion==1)})
+  
+}
+
+runApp(list(ui=ui,server=server))
 
 
