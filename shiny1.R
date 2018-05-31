ddc78bab83246ec4f6ef564abef9da1d9b587c62
unknown <cyukoy89@gmail.com>
2018-05-31 08:32:35 -0300

    agregamos titulo y numero de estacion

--- shiny1.R
+++ shiny1.R
@@ -1,7 +1,6 @@
 library(shiny)
 library(tidyverse)
 library(lubridate)
-library(interval)
 
 ui <- fluidPage(
   titlePanel("Temperaturas del Uru"),
@@ -14,8 +13,14 @@ ui <- fluidPage(
               label = "Año",
               min =2004,
               max = 2015,
-              value =c( 2002,2004)
+              value =c( 2002:2004)
   ),
+  sliderInput(inputId ="m",
+              label = "superior",
+              min = -10,
+              max=30,
+              value=12,
+              step= 0.2 ),
   plotOutput("serie")
 )
 
@@ -26,7 +31,10 @@ server <- function(input,output){
     temperaturas <- mutate(temperaturas, fecha=paste(dia,mes,anio, sep="-"))
     temperaturas <- mutate(temperaturas,fecha=dmy(temperaturas$fecha))
     temperaturas %>% filter(nroEstacion==input$est) %>% filter(between(anio,min(input$year),max(input$year))) %>%
-      ggplot(aes(x=fecha,y=tmin,colour=as.factor(anio))) + geom_point()
+      ggplot(aes(x=fecha,y=tmin,colour=as.factor(anio))) + geom_point()+geom_hline(yintercept = input$m, colour="red",size=2)+
+      ggtitle(paste("Temperaturas de la estación N°",input$est))
+    
+    
     
     
     
@@ -35,6 +43,4 @@ server <- function(input,output){
 
 
 shinyApp(ui, server)
-
-
 
