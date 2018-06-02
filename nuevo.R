c0c911b509d177d77e6192c41f810b63e23f91ff
Yuko <cyukoy89@gmail.com>
2018-06-02 00:00:19 -0300

    agregue orejitas, y una salida rmd, hay que bajar el arch rmd tambien

--- nuevo.R
+++ nuevo.R
@@ -1,33 +1,52 @@
 library(shiny)
 library(tidyverse)
 library(lubridate)
+library(rmarkdown)
 
 
-ui <- fluidPage(
-  titlePanel("Temperaturas del Uru"),
-  sliderInput(inputId = "est",
-              label = "Número de estación",
-              min = 1,
-              max = 26,
-              value=2),
-  sliderInput(inputId = "year",
-              label = "Año",
-              min =2004,
-              max = 2015,
-              value =c( 2002:2004)
-  ),
-  sliderInput(inputId ="m",
-              label = "superior",
-              min = -10,
-              max=30,
-              value=12,
-              step= 0.2 ),
-  plotOutput("serie")
+ui <- navbarPage(
+  title="Temperaturas mínimas del Uruguay",
   
+  tabPanel("Visualizaciones",type="tabset",
+           sidebarLayout(position = "right",
+                         sidebarPanel(
+                           
+                           sliderInput(inputId = "est",
+                                       label = "Número de estación",
+                                       min = 1,
+                                       max = 26,
+                                       value=2),
+                           sliderInput(inputId = "year",
+                                       label = "Año",
+                                       min =2004,
+                                       max = 2015,
+                                       value =c( 2002:2004)
+                           ),
+                           sliderInput(inputId ="m",
+                                       label = "superior",
+                                       min = -10,
+                                       max=30,
+                                       value=12,
+                                       step= 0.2 )),
+                         
+                         
+                         mainPanel(plotOutput("serie"))
+                         
+           )),
+  
+  tabPanel("Introducción",type="tabset",
+           fluidRow(
+             column(6,
+                    includeMarkdown("introduccion.Rmd")
+             ),
+             column(3,
+                    img(class="img-polaroid",
+                        src=paste0("https://media.elobservador.com.uy/adjuntos/181/imagenes/018/126/0018126280.jpeg")))))
 )
 
 
-server <- function(input,output){
+
+server <- function(input,output,session){
   output$serie <- renderPlot({
     temperaturas <- read.csv("temperaturas.csv",sep="\t")
     temperaturas <- mutate(temperaturas, fecha=paste(dia,mes,anio, sep="-"))
@@ -35,12 +54,12 @@ server <- function(input,output){
     temperaturas %>% filter(nroEstacion==input$est) %>% filter(between(anio,min(input$year),max(input$year))) %>%
       ggplot(aes(x=fecha,y=tmin,colour=as.factor(anio))) + geom_point()+geom_hline(yintercept = input$m, colour="red",size=2)+
       
-      ggtitle(paste("Temperaturas de la estación N°",input$est))
-    
-    ggtitle(paste("Temperaturas de la estación N°:",input$est))
+      ggtitle(paste("Temperaturas de la estación N°:",input$est))
     
   })
 }
-    
-    
+
+
 shinyApp(ui, server)
+
+
